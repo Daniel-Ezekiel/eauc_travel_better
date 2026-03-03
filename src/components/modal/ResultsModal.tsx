848a56c9e7a1402ca9fe74fc71b9036e6ef06fc6
Daniel Ezekiel <dbezekiel77@gmail.com>
2026-03-03 20:31:21 +0000

    feat: results now displayable after completing an assessment category

---
+++ src/components/modal/ResultsModal.tsx
@@ -0,0 +1,32 @@
+import { FaTimesCircle } from "react-icons/fa";
+
+export const ResultsModal = ({
+  score,
+  tag,
+  summary,
+  details,
+  handleClick,
+}: {
+  score: number;
+  tag: string;
+  summary: string;
+  details: string;
+  handleClick: () => void
+}) => {
+  return (
+    <div className="fixed top-0 left-0 w-dvw h-dvh p-4 grid place-items-center bg-midnight/50 backdrop-blur-xs">
+      <div className="relative max-w-180 max-h-[68dvh] z-10 p-8 grid gap-2 bg-midnight rounded-[1.25rem] rounded-br-none text-sandstone-tint overflow-y-scroll">
+        <h2 className="text-4xl text-center">Interpret your score</h2>
+        <p className="text-2xl">Score:  <span>{score}</span></p>
+        <div className="grid gap-4">
+            <h3>What a score between {tag} means:</h3>
+            <p className="font-medium">{summary}</p>
+            <p>{details}</p>
+        </div>
+      </div>
+      <button type="button" className="absolute flex gap-2 items-center top-10 right-10 z-10 cursor-pointer font-medium text-midnight" onClick={handleClick}>
+        <FaTimesCircle /> Close Results
+      </button>
+    </div>
+  );
+};
--- src/components/pages/Assessment.tsx
+++ src/components/pages/Assessment.tsx
@@ -1,43 +1,71 @@
 import { Link, useLocation } from "react-router";
 import { MainLayout } from "../layout/MainLayout";
 import AssessmentsConfig from "../../assets/assessments_config.json";
+import { useState } from "react";
+import { ResultsModal } from "../modal/ResultsModal";
 
-interface questions {
+interface Questions {
   id: string;
   text: string;
   scores: {
     disagree: number;
-    neutral: number,
-    agree: number,
-  }
+    neutral: number;
+    agree: number;
+  };
+}
+
+interface Outcome {
+  min_score: number;
+  max_score: number;
+  tag: string;
+  at_a_glance: string;
+  in_detail: string;
 }
 
 interface AssessmentCategory {
   id: string;
   title: string;
   description: string;
-  questions: questions[];
+  questions: Questions[];
+  outcomes: Outcome[];
 }
 
 export const Assessment = () => {
   const { pathname } = useLocation();
   const currPageId = pathname.slice(1);
 
+  const [assessmentScore, setAssessmentScore] = useState<number>(0);
+  const [toggleModal, setToggleModal] = useState<boolean>(false);
+  const [outcome, setOutcome] = useState<undefined | Outcome>(undefined);
+
   const [assessmentCategory]: AssessmentCategory[] =
     AssessmentsConfig.data.filter((assessment) => assessment.id === currPageId);
 
-  const currPageIdIndex: number = AssessmentsConfig.data.findIndex(assessment => assessment.id === currPageId)
-  const nextAssessmentId: string | undefined = AssessmentsConfig.data[currPageIdIndex + 1]?.id
+  const currPageIdIndex: number = AssessmentsConfig.data.findIndex(
+    (assessment) => assessment.id === currPageId,
+  );
+  const nextAssessmentId: string | undefined =
+    AssessmentsConfig.data[currPageIdIndex + 1]?.id;
 
   const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>): void => {
     const data = new FormData(e.currentTarget);
-    let output = "";
-    for (const entry of data) {
-      output = `${output}${entry[0]}=${entry[1]},\r`;
+    let totalScore = 0;
+    for (const [, score] of data) {
+      totalScore += +score;
     }
-    console.log(output);
+    setAssessmentScore(totalScore);
+
+    const validOutcome = assessmentCategory.outcomes.find(
+      (outcome) =>
+        totalScore >= outcome.min_score &&
+        totalScore <= outcome.max_score,
+    );
+    setOutcome(validOutcome);
+    setToggleModal(true)
+
+    console.log(validOutcome);
     e.preventDefault();
-  }
+  };
 
   return (
     <>
@@ -53,32 +81,75 @@ export const Assessment = () => {
           </div>
         </section>
         <section>
-          <form className="max-w-200 mx-auto grid gap-5" onSubmit={handleSubmit}>
+          <form
+            className="max-w-200 mx-auto grid gap-5"
+            onSubmit={handleSubmit}
+          >
             {assessmentCategory.questions.map((question) => (
-              <fieldset key={question.id} className="form-control flex flex-col gap-2">
-                <legend className="mb-2 w-full font-normal text-2xl">{question.id.slice(2)}. {question.text}</legend>
+              <fieldset
+                key={question.id}
+                className="form-control flex flex-col gap-2"
+              >
+                <legend className="mb-2 w-full font-normal text-2xl">
+                  {question.id.slice(2)}. {question.text}
+                </legend>
 
                 <div className="flex gap-2 justify-start items-center">
-                  <input type="radio" name={question.id} id={`${question.id}-disagree-choice`} value={question.scores.disagree} required />
-                  <label htmlFor={`${question.id}-disagree-choice`}>Disagree</label>
+                  <input
+                    type="radio"
+                    name={question.id}
+                    id={`${question.id}-disagree-choice`}
+                    value={question.scores.disagree}
+                    required
+                  />
+                  <label htmlFor={`${question.id}-disagree-choice`}>
+                    Disagree
+                  </label>
                 </div>
-                 <div className="flex gap-2 justify-start items-center">
-                  <input type="radio" name={question.id} id={`${question.id}-neutral-choice`} value={question.scores.neutral} />
-                  <label htmlFor={`${question.id}-neutral-choice`}>Neutral</label>
+                <div className="flex gap-2 justify-start items-center">
+                  <input
+                    type="radio"
+                    name={question.id}
+                    id={`${question.id}-neutral-choice`}
+                    value={question.scores.neutral}
+                  />
+                  <label htmlFor={`${question.id}-neutral-choice`}>
+                    Neutral
+                  </label>
                 </div>
-                 <div className="flex gap-2 justify-start items-center">
-                  <input type="radio" name={question.id} id={`${question.id}-agree-choice`} value={question.scores.agree} />
+                <div className="flex gap-2 justify-start items-center">
+                  <input
+                    type="radio"
+                    name={question.id}
+                    id={`${question.id}-agree-choice`}
+                    value={question.scores.agree}
+                  />
                   <label htmlFor={`${question.id}-agree-choice`}>Agree</label>
                 </div>
               </fieldset>
             ))}
 
             <div className="flex justify-between">
-              <button type="submit" className="cursor-pointer py-2 px-5 bg-midnight text-sandstone-tint rounded-l-full">See Results</button>
-              {nextAssessmentId && <Link to={`/${nextAssessmentId}`} className="cursor-pointer py-2 px-5 bg-midnight text-sandstone-tint rounded-r-full">Next Assessment</Link>}
+              <button
+                type="submit"
+                className="cursor-pointer py-2 px-5 bg-midnight text-sandstone-tint rounded-l-full"
+              >
+                See Results
+              </button>
+              {nextAssessmentId && (
+                <Link
+                  to={`/${nextAssessmentId}`}
+                  className="cursor-pointer py-2 px-5 bg-midnight text-sandstone-tint rounded-r-full"
+                >
+                  Next Assessment
+                </Link>
+              )}
             </div>
           </form>
         </section>
+        {
+          toggleModal && <ResultsModal score={assessmentScore} tag={(outcome as Outcome).tag} summary={(outcome as Outcome).at_a_glance} details={(outcome as Outcome).in_detail} handleClick={() => setToggleModal(false)} />
+        }
       </MainLayout>
     </>
   );
