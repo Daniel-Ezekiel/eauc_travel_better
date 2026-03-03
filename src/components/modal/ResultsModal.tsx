dbd2897280d9902790e76807bcddfbcafa035ee6
Daniel Ezekiel <dbezekiel77@gmail.com>
2026-03-03 20:34:29 +0000

    style: adjusted modal overlay background color

--- src/components/modal/ResultsModal.tsx
+++ src/components/modal/ResultsModal.tsx
@@ -14,7 +14,7 @@ export const ResultsModal = ({
   handleClick: () => void
 }) => {
   return (
-    <div className="fixed top-0 left-0 w-dvw h-dvh p-4 grid place-items-center bg-midnight/50 backdrop-blur-xs">
+    <div className="fixed top-0 left-0 w-dvw h-dvh p-4 grid place-items-center bg-midnight/70 backdrop-blur-xs">
       <div className="relative max-w-180 max-h-[68dvh] z-10 p-8 grid gap-2 bg-midnight rounded-[1.25rem] rounded-br-none text-sandstone-tint overflow-y-scroll">
         <h2 className="text-4xl text-center">Interpret your score</h2>
         <p className="text-2xl">Score:  <span>{score}</span></p>
@@ -24,7 +24,7 @@ export const ResultsModal = ({
             <p>{details}</p>
         </div>
       </div>
-      <button type="button" className="absolute flex gap-2 items-center top-10 right-10 z-10 cursor-pointer font-medium text-midnight" onClick={handleClick}>
+      <button type="button" className="absolute flex gap-2 items-center top-10 right-10 z-10 cursor-pointer font-medium text-sandstone-tint" onClick={handleClick}>
         <FaTimesCircle /> Close Results
       </button>
     </div>
